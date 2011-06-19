16e8a80e44e2f3d865ee7daf122ce8211926c21b
Mox Caml <icfp2011@starlancer.org>
2011-06-19 09:22:27 -0400

    Change run_exp as a agressive algorithm.

--- src/run_exp.ml
+++ src/run_exp.ml
@@ -5,10 +5,6 @@ open Command
 ;;
 
 let controller _ =
-  revive_any_dead
-    (fun _ ->
-      heal_damaged
-      	(fun _ ->
-	  zombienize
-	    (fun _ -> normal_attack nop))) in
+  zombienize
+    (fun _ -> normal_attack nop) in
 run_simulator controller
